f0c5815d54eff06dbb6bb4177669fab1ce73f175
Mandella-thi <thiagogarc@gmail.com>
2020-11-03 21:34:58 -0300

    pagina java

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function clicou() {
+    alert("obrigado por clicar");
+    
+} 
+
+/*var nome = "Thiago Garcia";
+var n1 = 35;
+var n2 =10;
+var frase = "Japão é o melhor time do mundo"
+alert (nome +"tem " +n1 + "anos" +n1*n2);//comentario
+console.log(nome +n1*n2)
+console.log(frase.replace("Japão", "Brasil"));*/
